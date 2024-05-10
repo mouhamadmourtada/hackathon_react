3af9f248ce42f4244aa10038ce43ae50d3efd19d
saliougit <username@users.noreply.github.com>
2024-05-10 21:50:30 +0000

    Image component

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,7 +1,17 @@
 //Image (avec props le type, est-ce que ce sera une image arrondi etc....)
 
+/**
+ * 
+ * @param {url} string 
+ *  
+ 
+ */
 
-export default function  Image(){
+export default function  Image({url, type}){
+    return (
+        <img src={url} alt="image" className={type} />
+    )
+
 
 
 }
